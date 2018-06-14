259b35d7f826c6cea5bb3392c6de88be33257dc1
AngelCobian <angelcobian@outlook.com>
2018-06-13 17:00:27 -0700

    broken code!!

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,8 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class AppComponent implements OnInit {
   title = 'app';
   monsterName: string = 'kaiju';
+  monsterPower: number;
 
-  ngOnInit(){
-
+  hero: any ={
+    name: '',
+    power: null
   }
+
+  monsters: any[] = [
+    {
+      monsterName: 'kaiju',
+      monsterPower: null
+    },
+    {
+      monsterName: 'smaug',
+    monsterPower: null
+  }
+  ];
+  monsterSelected;
+
+  heroPower(){
+    this.hero.power = Math.floor(Math.random() * 100);
+  }
+
+generatePower() {
+ this.monsterSelected.monsterPower = Math.floor(Math.random() * 101);
+}
+
+  ngOnInit() {
+    this.pickMonster();
+}
+pickMonster() {
+  const index = Math.floor(Math.random() * this.monsters.length);
+  this.monsterSelected = this.monsters[index];
+}
+
 }
